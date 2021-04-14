b4d03c5183ca6c2d9d25b8d67da3020d263c1328
Shwetha Nayak <64678982+shwethanayak-lang@users.noreply.github.com>
2021-04-14 17:11:05 +0530

    Create Ph1L3.js

---
+++ Ph1L3.js
@@ -0,0 +1,29 @@
+
+   
+    var addUser = () => {
+     //Email Id verification
+      var x = document.myform.email.value;
+      var atposition = x.indexOf("@");
+      var dotposition = x.lastIndexOf(".");
+      if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        document.getElementById("errors").innerHTML = "Please enter a valid e-mail address"; // Indicate the error message
+      }
+      else{
+        // Display the entries
+      var username = document.getElementById("name").value;
+      var emailid = document.getElementById("email").value;
+      document.getElementById("Name").innerHTML = username;
+      document.getElementById("Email").innerHTML = emailid;
+      }
+      //  console.log("UserName:" + username + "   "  +  "EmailId:" +  emailid);
+    }
+
+    var deleteEntry = (Name, email) => {
+      document.getElementById("Name").innerHTML = "";
+      document.getElementById("Email").innerHTML = "";
+        document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+    }
+
+
+  
